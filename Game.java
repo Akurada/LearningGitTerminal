5c0f79f7ea9581a5119d473476dcb7eabb0a402b
Abhinav Kurada <abhinav.kurada@gmail.com>
2016-06-20 15:58:55 -0400

    Jun 20 4 PM

--- Game.java
+++ Game.java
@@ -1,9 +1,20 @@
+import java.io.Console;
 
 public class Game {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		System.out.println("Welcome to the capitals game");
+		
+		Console console = System.console();
+		String answer = console.readLine("What is the capital of Australia");
+		String capital = "Canberra";
+		if(answer.equals(capital)){
+			System.out.println("Correct!");
+		}
+		else {
+			System.out.println("No the capital is " + capital);
+		}
 
 	}
 
