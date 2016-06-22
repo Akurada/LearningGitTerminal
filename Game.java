0ab9ed99dd39baad252d71caa2200ee672b518b7
Abhinav Kurada <abhinav.kurada@gmail.com>
2016-06-22 14:02:11 -0400

    fixed array null pointer exception resulting from extra commas in the csv file

--- Game.java
+++ Game.java
@@ -3,75 +3,37 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import java.util.Random;
-import java.util.LinkedList;
 
 class Game {
-
-	public static void printHelp() {
-		System.out.println("Guess the capital city");
-		System.out.println("type 'EXIT' to stop the game, or 'HELP' to display this text.");
-	}
-
-
 	public static void main(String[] args) {
-
+		System.out.println("Capitals game");
 		Console console = System.console();
-
 		int total = 0;
 		int correct = 0;
-		Random r = new Random();
-		LinkedList<String> questions = new LinkedList<String>();
+		
 		try (BufferedReader br = new BufferedReader(new FileReader("capitals2.csv"));) {
-			
+		
 			String line;
 			while ((line = br.readLine()) != null) {
-				questions.add(line);
+				String[] cols = line.split(",");
+				String country = cols[0];
+				String capital = cols[1];
+				String answer = console.readLine("What is the capital of " + country + "? ");
+				total++;
+				if (answer.equals(capital)) {
+					System.out.println("Correct!");
+					correct++;
+				}
+				else {
+					System.out.println("No, the answer is " + capital + "!");	
 				}
 			}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
+		}
+	catch (FileNotFoundException e) {
+		e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
+		e.printStackTrace();
 		}
-
-				
-		while (!questions.isEmpty()) {
-			String line = questions.remove(0);
-			String[] cols = line.split(",");
-			String country = cols[0];
-			String capital = cols[1];
-			String answer = null;				
-			do {
-				answer = console.readLine("What is the capital of " + country + "? ");
-				if (answer.equalsIgnoreCase("help")) {
-					printHelp();
-				}
-			}
-			while (answer.equalsIgnoreCase("help"));
-			if (answer.equalsIgnoreCase("exit")) {
-				break;
-			}
-			if (answer.equalsIgnoreCase(capital)) {
-				switch (r.nextInt() % 3) {
-					case 0:
-						System.out.println("Well done!");
-						break;
-					case 1:
-						System.out.println("That's right!");
-						break;
-					default:
-						System.out.println("Correct!");
-						break;
-					}
-					
-				correct++;
-			}
-			else {
-				System.out.println("No, the answer is " + capital);	
-			}
-			total++;
-		}
-	System.out.println("You scored " + correct + " out of " + total);
+		System.out.println("You scored " + correct + " out of " + total);
 	}
 }
